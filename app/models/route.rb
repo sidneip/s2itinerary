2fbc84945900b6b7ef334bb5c2ca9bc53a9c4b1f
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-01 20:55:20 -0200

    uniq route

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,5 +2,5 @@ class Route < ActiveRecord::Base
   belongs_to :map
   validates :origin, :destination, :distance, :map, presence: true
   validates :distance, numericality: { greater_than: 0 }
-  validates :origin, :uniqueness => {:scope => :destination}
+  validates :origin, uniqueness: { scope: [:destination, :map_id] }
 end
