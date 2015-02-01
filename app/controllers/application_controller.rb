bf5aa4d4bf9ed9f407965211b9c9f5bbe4913f0b
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-01 20:51:33 -0200

    rescue errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: {exception.param => "is required"}, status: 422
+  end
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: {error: exception.message}, status: 422
+  end
 end
