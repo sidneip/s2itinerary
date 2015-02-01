5284a853087adf4d244d7966bc7b3617e6237ed1
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-01 21:59:16 -0200

    method return best route

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,12 @@
 class Map < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :routes
+
+  def best_route(origin, destination)
+    routes = []
+    Map.last.routes.each do |route|
+      routes << [route.origin, route.destination, route.distance.round]
+    end
+    Graph.new(routes).shortest_path(origin, destination)
+  end
 end
