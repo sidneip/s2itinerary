644547d87d318bba2805446f90d6d7b3341bae09
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-01 20:55:41 -0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201162414) do
+ActiveRecord::Schema.define(version: 20150201174017) do
 
   create_table "maps", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "routes", force: true do |t|
+    t.string   "origin"
+    t.string   "destination"
+    t.decimal  "distance"
+    t.integer  "map_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "routes", ["map_id"], name: "index_routes_on_map_id"
 
 end
