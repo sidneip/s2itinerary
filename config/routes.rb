dbe9e1a6763a669bf7d30af8e8cb4fd90d84850f
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-01 21:59:29 -0200

    add route to deliveries

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       post 'map' => 'maps#create', as: :maps_create
       post 'route' => 'routes#create', as: :routes_create
+      get 'delivery' => 'deliveries#search', as: :deliveries_search
     end
   end
 end
