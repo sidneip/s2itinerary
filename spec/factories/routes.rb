ea9891cb48aec0a987d63172fa3efa1476a5388b
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-02 17:09:26 -0200

    add routes

--- spec/factories/routes.rb
+++ spec/factories/routes.rb
@@ -5,4 +5,39 @@ FactoryGirl.define do
     distance '10'
     association :map, factory: :map
   end
+
+  factory :route2 do
+    origin 'B'
+    destination 'D'
+    distance '15'
+    association :map, factory: :map
+  end
+
+  factory :route3 do
+    origin 'A'
+    destination 'C'
+    distance '20'
+    association :map, factory: :map
+  end
+
+  factory :route4 do
+    origin 'C'
+    destination 'D'
+    distance '30'
+    association :map, factory: :map
+  end
+
+  factory :route5 do
+    origin 'B'
+    destination 'E'
+    distance '50'
+    association :map, factory: :map
+  end
+
+  factory :route6 do
+    origin 'D'
+    destination 'E'
+    distance '30'
+    association :map, factory: :map
+  end
 end
