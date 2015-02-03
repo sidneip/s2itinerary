8f009e8689a091bf7acb381c172b1ef98dde759b
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-03 12:13:03 -0200

    Add get maps and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  namespace :api, path: '' do
+  namespace :api, path: '', format: 'json' do
     namespace 'v1' do
+      #map
+      get  "/map/:name" => 'maps#show', as: :maps_show
       post 'map' => 'maps#create', as: :maps_create
+      #route
+      get  'route' => "routes#show", as: :routes_show
       post 'route' => 'routes#create', as: :routes_create
+      #delivery
       get 'delivery' => 'deliveries#search', as: :deliveries_search
     end
   end
