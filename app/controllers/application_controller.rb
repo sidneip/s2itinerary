e47186c9fbf75065ed6a8c710b129a3e3a315e8a
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-03 18:52:37 -0200

    translate many messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
 
-  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   rescue_from ActionController::ParameterMissing do |exception|
-    render json: {exception.param => "is required"}, status: 422
+    render json: { exception.param => t('required') }, status: 422
   end
 
   rescue_from ActiveRecord::RecordInvalid do |exception|
-    render json: {error: exception.message}, status: 422
+    render json: { error: exception.message }, status: 422
   end
 
   def not_found
-    render json: { errors: 'not found' }, status: :not_found 
+    render json: { errors: t('error_404_message') }, status: :not_found
   end
-
 end
