7e811f33eaa75b68fc6e4580d86af765f652a4a8
Sidnei Pacheco <sidnei.oliveira@abril.com.br>
2015-02-05 15:32:56 -0200

    Refactor delegate method object model correct

--- app/controllers/api/v1/deliveries_controller.rb
+++ app/controllers/api/v1/deliveries_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::DeliveriesController < ApplicationController
   def search
     @map = Map.find_by(name: params[:map])
     render json: { errors: 'not found' }, status: :not_found unless @map.present?
-    best_route, distance = @map.best_route(params[:origin], params[:destination])
-    cost = @map.calculate_cost(distance, params[:efficiency], params[:cost])
+    delivery = Delivery.new(params[:origin], params[:destination], @map)
+    best_route, distance = delivery.best_route
+    cost = delivery.calculate_cost(distance, params[:efficiency], params[:cost])
     render json: { route: best_route, distance: distance, cost: cost}, status: :ok
   end
 
